47a05eaf0c25f94f4dd3047e3241f86b89095e36
seajensen <42650494+seajensen@users.noreply.github.com>
2020-11-12 16:55:05 -0700

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+const toggleMenu = () => {
+    document.getElementById('hmbg-menu').classList.toggle('open')
+    console.log(document.getElementById('nav-menu'))
+    document.getElementById('nav-menu').classList.toggle("open");
+}
